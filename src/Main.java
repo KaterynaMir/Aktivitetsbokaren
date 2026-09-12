651db94a00af6c7d3fda39f7600b1744f8a281cb
Kateryna <kateryna@Mac.home>
2026-09-12 20:48:30 +0200

    add registerBooking method

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static final String[] ACTIVITIES = {"Programmeringsworkshop","Matlagningskurs","Träningspass"};
     public static final int[] PRICES = {200, 300, 150};
+    public static final int MAX_NUM_PARTICIPANTS = 500;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
--- src/Menu.java
+++ src/Menu.java
@@ -20,7 +20,7 @@ public class Menu {
         boolean continueAction = true;
         int choice = InputReader.readIntWithCondition(scan, "Ange ditt val: ", 1, 4);
         switch (choice) {
-            case 1 -> System.out.println("registerBooking()");
+            case 1 -> registerBooking(scan);
             case 2 -> System.out.println("showAllaBookings()");
             case 3 -> System.out.println("showSummary()");
             case 4 -> {
@@ -29,5 +29,31 @@ public class Menu {
             }
         }
         return continueAction;
+    }
+
+    static void registerBooking(Scanner scan) {
+        printActivities();
+        int activityIndex =InputReader.readIntWithCondition(scan,"Aktivitet? ",
+                1, Main.ACTIVITIES.length) - 1;
+        int numOfParticipants = InputReader.readIntWithCondition(scan,"Antal deltagare? ",
+                1, Main.MAX_NUM_PARTICIPANTS);
+
+        int price = calculateBookingPrice(activityIndex, numOfParticipants);
+        System.out.println("Registrerad en ny bokning:");
+        System.out.println(Main.ACTIVITIES[activityIndex] + ", " + numOfParticipants + " deltagare, " + price + " kr");
+    }
+
+    static void printActivities() {
+        System.out.println("\n------------------------");
+        System.out.println("AKTIVITETER:");
+        System.out.println("------------------------");
+        for (int i = 0; i < Main.ACTIVITIES.length; i++) {
+            System.out.format("%2d %-25s %4d\n", i + 1, Main.ACTIVITIES[i], Main.PRICES[i]);
+        }
+        System.out.println("------------------------\n");
+    }
+
+    static int calculateBookingPrice (int activityIndex, int numOfParticipants){
+        return Main.PRICES[activityIndex] * numOfParticipants;
     }
 }
